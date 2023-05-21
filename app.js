0d09dfd44d96c51b67d6dbc6e3ad47fab2523e52
nndang27 <97721662+nndang27@users.noreply.github.com>
2023-05-21 10:44:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -462,8 +462,8 @@ const createWebRtcTransport = async (router) => {
       const webRtcTransport_options = {
         listenIps: [
           {
-            ip: '0.0.0.0', // replace with relevant IP address
-            announcedIp: '10.0.0.115',
+            ip: process.env.MEDIASOUP_LISTEN_IP || '0.0.0.0', // replace with relevant IP address
+            announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP,
           }
         ],
         enableUdp: true,
